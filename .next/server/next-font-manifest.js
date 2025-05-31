ef87c2bee607edbc04d72886b34c6203edcaec79
Ashwin Kulkarni <ashwin.kulkarni128@gmail.com>
2025-05-31 14:02:56 +0530

    chore: update gitignore and remove tracked build files

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ashwin/Documents/Projects/OSS/utilities/src/app/layout\":[\"static/media/5d258b355fefebcb-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+00ekF+MNHSIaoenzxpauMN5oDvW4BW+Jte9kZM83/0=\"\n}"
