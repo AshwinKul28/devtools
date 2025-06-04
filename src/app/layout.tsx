3e34186840bb4c7101705fe9eaf62447d5b08e1b
Ashwin Kulkarni <ashwin.kulkarni128@gmail.com>
2025-06-04 11:42:58 +0530

    added icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -178,6 +178,14 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1711684120101178"
           crossOrigin="anonymous"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <meta name="theme-color" content="#0ea5e9" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
